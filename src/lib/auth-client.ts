36afce6edf9bc7e57c3899fe1a91cd87b835147e
mtj-mjc <matej.mrnjec@exanic.ch>
2025-11-14 15:52:59 +0100

    fix: env example

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,7 +3,7 @@ import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.PUBLIC_URL ?? "http://localhost:3000",
   plugins: [genericOAuthClient()],
 });
 
