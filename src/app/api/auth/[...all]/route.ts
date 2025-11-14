29893387ec89edf67f6b390fe739eb8f0b753dbd
mtj-mjc <matej.mrnjec@exanic.ch>
2025-11-14 16:02:44 +0100

    fix: remove comments

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -2,25 +2,3 @@ import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
 export const { POST, GET } = toNextJsHandler(auth);
-
-// const { data, error } = await authClient.signIn.email({
-//         /**
-//          * The user email
-//          */
-//         email,
-//         /**
-//          * The user password
-//          */
-//         password,
-//         /**
-//          * A URL to redirect to after the user verifies their email (optional)
-//          */
-//         callbackURL: "/dashboard",
-//         /**
-//          * remember the user session after the browser is closed.
-//          * @default true
-//          */
-//         rememberMe: false
-// }, {
-//     //callbacks
-// })
