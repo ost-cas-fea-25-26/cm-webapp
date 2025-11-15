2b892d2a386dc16dcd98c2431a5e45dc4bb244c8
mtj-mjc <matej.mrnjec@exanic.ch>
2025-11-15 11:00:06 +0100

    fix: urls

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,8 +3,8 @@ import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`
     : "http://localhost:3000",
   plugins: [genericOAuthClient()],
 });
@@ -12,6 +12,7 @@ export const authClient = createAuthClient({
 console.warn(process.env.VERCEL_URL);
 console.warn(process.env.VERCEL_BRANCH_URL);
 console.warn(process.env.NEXT_PUBLIC_VERCEL_URL);
+console.warn(process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL);
 
 export const { signIn, signOut, useSession } = authClient;
 
