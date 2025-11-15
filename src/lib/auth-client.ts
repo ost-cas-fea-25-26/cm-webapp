3a031a9534223b5bcbfbb425c31b2a77153dc427
mtj-mjc <matej.mrnjec@exanic.ch>
2025-11-15 10:56:38 +0100

    fix: env vars

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,10 +3,9 @@ import { genericOAuthClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL:
-    (process.env.VERCEL_URL ?? process.env.VERCEL_BRANCH_URL)
-      ? `https://${process.env.VERCEL_URL}`
-      : "https://cm-webapp-git-feature-citurso-matej-mrnjecs-projects.vercel.app/",
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000",
   plugins: [genericOAuthClient()],
 });
 
