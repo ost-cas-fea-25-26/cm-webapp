f9531f85be8a22ea52ccc424e2fe2338e581e9d7
mtj-mjc <matej.mrnjec@exanic.ch>
2025-11-15 11:23:49 +0100

    fix: format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getSession } from "@/lib/auth";
 
 export default async function Home() {
   const session = await getSession();
-  
+
   return (
     <main>
       <h1>Hello In Mumble</h1>
