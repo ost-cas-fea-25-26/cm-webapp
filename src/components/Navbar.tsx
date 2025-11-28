3d1fce9bc638dc11055108a66ba2da5bc92d995b
Carla Iten <carla.iten@apptiva.ch>
2025-11-28 16:54:06 +0100

    updated design system dependency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,23 @@
 "use client";
 
+import { LogoLink } from "@krrli/cm-designsystem";
+
 const Navbar = () => {
   return (
-    <nav
-      className="fixed col-start-2 col-end-3 px-4 top-0  left-1/2 -translate-x-1/2 z-50 bg-violet-600 w-full max-w-[60ch] h-20 flex items-center justify-between "
-      // style={{ height: "80px" }}
-    >
-      <div className="text-white font-semibold text-lg">Logo here</div>
-      <div className="flex items-center gap-4 text-white">
-        <div className="text-sm">Person goes here</div>
-        <button className="text-sm bg-transparent">Settings</button>
-        <button className="text-sm bg-transparent">Log Out</button>
-      </div>
-    </nav>
+    <div className="col-span-3 bg-violet-600 h-[80px]">
+      <nav className="col-start-2 col-end-3 h-[80px] flex items-center justify-between bg-transparent px-8 py-3 gap-2 opacity-100">
+        {/* Left: Logo */}
+        <div className="text-white font-semibold text-lg">
+          <LogoLink />
+        </div>
+        {/* Right: User Actions */}
+        <div className="flex items-center gap-2 text-white">
+          <div className="text-sm">Person goes here</div>
+          <button className="text-sm bg-transparent">Settings</button>
+          <button className="text-sm bg-transparent">Log Out</button>
+        </div>
+      </nav>
+    </div>
   );
 };
 
