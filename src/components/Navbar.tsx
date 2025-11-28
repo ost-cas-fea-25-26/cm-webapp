ccb8f3afce41bbb46483c6042cb3c04044f3b36e
Carla Iten <carla.iten@apptiva.ch>
2025-11-28 16:33:38 +0100

    reintroduced tailwind

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@
 const Navbar = () => {
   return (
     <nav
-      className="fixed top-0 left-1/2 -translate-x-1/2 z-50 bg-violet-600 w-full max-w-[60ch] h-20 flex items-center justify-between px-4"
-      style={{ height: "80px" }}
+      className="fixed col-start-2 col-end-3 px-4 top-0  left-1/2 -translate-x-1/2 z-50 bg-violet-600 w-full max-w-[60ch] h-20 flex items-center justify-between "
+      // style={{ height: "80px" }}
     >
       <div className="text-white font-semibold text-lg">Logo here</div>
       <div className="flex items-center gap-4 text-white">
