f58ecdbc0090567fdb0e7375981c8bfa1b461087
Carla Iten <carla.iten@apptiva.ch>
2025-12-18 15:33:32 +0100

    before i got distracted =D

--- src/app/(protected)/posts/[id]/page.tsx
+++ src/app/(protected)/posts/[id]/page.tsx
@@ -1,8 +1,31 @@
+import { Post as PostComponent } from "@krrli/cm-designsystem";
+
+
 type PostDetailPageProps = {
   params: { id: string };
 };
 
 export default async function PostDetailPage({ params }: PostDetailPageProps) {
-  const postId = (await params).id;
-  return <span>Post detail of post with id: {postId}</span>;
+
+  const postId = params.id;
+
+    return (
+      <p> post detail of post {postId}</p>
+      // <PostComponent
+      //   size="md"
+      //   displayName={props.post.creator?.displayName ?? undefined}
+      //   userName={props.post.creator?.username ?? undefined}
+      //   timestamp={postTimeStamp}
+      //   text={props.post.text ?? undefined}
+      //   avatarSrc={props.post.creator?.avatarUrl ?? undefined}
+      //   imageSrc={props.post.mediaUrl ?? undefined}
+      //   nbrOfComments={props.post.replies ?? 0}
+      //   nbrOfLikes={props.post.likes ?? 0}
+      //   onAvatarClick={goToProfilePage}
+      //   onCommentClick={goToPostDetailPage}
+      //   onLikeClick={onLikeButtonClick}
+      //   onShareClick={copyLinkToClipboard}
+      //   detailLink={postDetailPageUrl}
+      // ></PostComponent>
+    );
 }
