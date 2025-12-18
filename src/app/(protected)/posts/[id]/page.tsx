88dc4d25022f544980785941d2b6dbad9ec05af6
Carla Iten <carla.iten@apptiva.ch>
2025-12-18 16:04:28 +0100

    thanks prettier

--- src/app/(protected)/posts/[id]/page.tsx
+++ src/app/(protected)/posts/[id]/page.tsx
@@ -1,31 +1,29 @@
 import { Post as PostComponent } from "@krrli/cm-designsystem";
 
-
 type PostDetailPageProps = {
   params: { id: string };
 };
 
 export default async function PostDetailPage({ params }: PostDetailPageProps) {
-
   const postId = params.id;
 
-    return (
-      <p> post detail of post {postId}</p>
-      // <PostComponent
-      //   size="md"
-      //   displayName={props.post.creator?.displayName ?? undefined}
-      //   userName={props.post.creator?.username ?? undefined}
-      //   timestamp={postTimeStamp}
-      //   text={props.post.text ?? undefined}
-      //   avatarSrc={props.post.creator?.avatarUrl ?? undefined}
-      //   imageSrc={props.post.mediaUrl ?? undefined}
-      //   nbrOfComments={props.post.replies ?? 0}
-      //   nbrOfLikes={props.post.likes ?? 0}
-      //   onAvatarClick={goToProfilePage}
-      //   onCommentClick={goToPostDetailPage}
-      //   onLikeClick={onLikeButtonClick}
-      //   onShareClick={copyLinkToClipboard}
-      //   detailLink={postDetailPageUrl}
-      // ></PostComponent>
-    );
+  return (
+    <p> post detail of post {postId}</p>
+    // <PostComponent
+    //   size="md"
+    //   displayName={props.post.creator?.displayName ?? undefined}
+    //   userName={props.post.creator?.username ?? undefined}
+    //   timestamp={postTimeStamp}
+    //   text={props.post.text ?? undefined}
+    //   avatarSrc={props.post.creator?.avatarUrl ?? undefined}
+    //   imageSrc={props.post.mediaUrl ?? undefined}
+    //   nbrOfComments={props.post.replies ?? 0}
+    //   nbrOfLikes={props.post.likes ?? 0}
+    //   onAvatarClick={goToProfilePage}
+    //   onCommentClick={goToPostDetailPage}
+    //   onLikeClick={onLikeButtonClick}
+    //   onShareClick={copyLinkToClipboard}
+    //   detailLink={postDetailPageUrl}
+    // ></PostComponent>
+  );
 }
