d7ee88d6da9ac363949d661d21cfa8e26f362d2f
Carla Iten <carla.iten@apptiva.ch>
2025-12-29 23:16:22 +0100

    add modal to settings button

--- src/components/base/MumbleSettingsButton.tsx
+++ src/components/base/MumbleSettingsButton.tsx
@@ -1,6 +1,16 @@
 "use client";
 
-import { NaviButton, Settings } from "@krrli/cm-designsystem";
+import {
+  Button,
+  Cancel,
+  Checkmark,
+  Modal,
+  ModalActions,
+  ModalBody,
+  NaviButton,
+  Settings,
+} from "@krrli/cm-designsystem";
+import { useState } from "react";
 import { tv } from "tailwind-variants";
 
 const loginButtonStyles = tv({
@@ -12,15 +22,52 @@ const loginButtonStyles = tv({
 
 const MumbleSettingsButton = () => {
   const { base, icon } = loginButtonStyles();
+  const [settingsModalOpen, setSettingsModalOpen] = useState(false);
   return (
-    <NaviButton
-      className={base()}
-      icon={Settings}
-      iconClassName={icon()}
-      intent="secondary"
-    >
-      Settings
-    </NaviButton>
+    <>
+      <NaviButton
+        className={base()}
+        icon={Settings}
+        iconClassName={icon()}
+        intent="secondary"
+        onClick={() => {
+          setSettingsModalOpen(true);
+        }}
+      >
+        Settings
+      </NaviButton>
+      <Modal
+        onOpenChange={() => {
+          setSettingsModalOpen(!settingsModalOpen);
+        }}
+        title="Modal"
+        open={settingsModalOpen}
+      >
+        <ModalBody>Hello, this is a Modal!!!</ModalBody>
+        <ModalActions>
+          <Button
+            icon={Cancel}
+            intent="primary"
+            onClick={() => {
+              setSettingsModalOpen(false);
+            }}
+            size="md"
+          >
+            Cancel
+          </Button>
+          <Button
+            icon={Checkmark}
+            intent="secondary"
+            onClick={() => {
+              console.log("Save clicked");
+            }}
+            size="md"
+          >
+            Save
+          </Button>
+        </ModalActions>
+      </Modal>
+    </>
   );
 };
 
