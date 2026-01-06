261f3860ce80979363a1b14f223f64c3e3550fed
Carla Iten <carla.iten@apptiva.ch>
2026-01-06 15:10:02 +0100

    added PostRepliesSection test

--- src/test-utils/createMockPost.ts
+++ src/test-utils/createMockPost.ts
@@ -1,4 +1,4 @@
-import { Post } from "@/lib/api/posts/post.types";
+import { Post, Reply } from "@/lib/api/posts/post.types";
 
 export const createMockPost = (overrides: Partial<Post> = {}): Post =>
   ({
@@ -7,3 +7,17 @@ export const createMockPost = (overrides: Partial<Post> = {}): Post =>
     avatar: "test-avatar-url",
     ...overrides,
   }) as Post;
+
+export const createMockReply = (overrides: Partial<Reply> = {}): Reply =>
+  ({
+    id: "reply-post-id",
+    parentId: "parent-of-reply-id",
+    text: "Das ist eine sehr wuetende Antwort!11!elf",
+    creator: {
+      id: "reply-creator-id",
+      username: "wutbuerger",
+    },
+    likedBySelf: false,
+    likeCount: 0,
+    ...overrides,
+  }) as Reply;
