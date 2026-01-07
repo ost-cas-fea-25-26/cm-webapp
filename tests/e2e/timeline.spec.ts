c529b80ee2e0bf25c52b442802670ef84bbf9391
Carla Iten <carla.iten@apptiva.ch>
2026-01-07 18:23:35 +0100

    update todos in timeline

--- tests/e2e/timeline.spec.ts
+++ tests/e2e/timeline.spec.ts
@@ -1,12 +1,18 @@
 import { test, expect } from "@playwright/test";
 
-test("Timeline zeigt Daten nach Login", async ({ page }) => {
-  // Du startest direkt hier, ohne Redirect!
+test("Timeline shows data after login", async ({ page }) => {
   await page.goto("/");
 
   const timeline = page.getByTestId("timeline-page");
   await expect(timeline).toBeVisible();
 
-  // Prüfen, ob der PostCreator geladen ist
+  // check if post creator is visible (todo: improve by checking for test id)
   await expect(page.getByRole("textbox")).toBeVisible();
+
+  /**
+   * todo:
+   * - check if post can be created
+   * - check if posts are visible in feed
+   * - check if logout works (and redirect to login page - is that possible with this setup?)
+   */
 });
