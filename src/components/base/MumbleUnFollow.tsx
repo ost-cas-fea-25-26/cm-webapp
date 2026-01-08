f42804f94775503cdd152116cad01ed2fef688af
Matej Mrnjec <45061360+mtj-mjc@users.noreply.github.com>
2026-01-08 23:30:51 +0100

    fix: add loading handle in follow/unfollow (#140)

--- src/components/base/MumbleUnFollow.tsx
+++ src/components/base/MumbleUnFollow.tsx
@@ -3,7 +3,9 @@
 import { unfollowUser } from "@/actions/user.action";
 import { Button, Cancel, Label } from "@krrli/cm-designsystem";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { tv } from "tailwind-variants";
+import MumbleLoading from "./MumbleLoading";
 
 const mumbleUnFollowStyles = tv({
   slots: {
@@ -20,24 +22,30 @@ export type MumbleUnFollowProps = {
 const MumbleUnFollow = (props: MumbleUnFollowProps) => {
   const { base, button } = mumbleUnFollowStyles();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className={base()}>
       <Label as="span" size="md">
         You follow {props.displayName}
       </Label>
-      <Button
-        size="md"
-        intent="primary"
-        icon={Cancel}
-        className={button()}
-        onClick={async () => {
-          await unfollowUser(props.userId);
-          router.refresh();
-        }}
-      >
-        Unfollow
-      </Button>
+      {loading ? (
+        <MumbleLoading />
+      ) : (
+        <Button
+          size="md"
+          intent="primary"
+          icon={Cancel}
+          className={button()}
+          onClick={async () => {
+            setLoading(true);
+            await unfollowUser(props.userId);
+            router.refresh();
+          }}
+        >
+          Unfollow
+        </Button>
+      )}
     </div>
   );
 };
