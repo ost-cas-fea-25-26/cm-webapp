3e66d51d39c2a63b8203cad20c0aeea155875e16
Matej Mrnjec <45061360+mtj-mjc@users.noreply.github.com>
2026-01-08 17:37:10 +0100

    feat: add biography example text (#136)
    Add example text for biography, so it can be seen that the support
    already exists.

--- src/components/base/MumbleProfileBanner.tsx
+++ src/components/base/MumbleProfileBanner.tsx
@@ -26,6 +26,7 @@ const MumbleProfileBanner = (props: MumbleProfileBannerProps) => {
         await updateAvatarAction(props.user.id!, file);
         router.refresh();
       }}
+      description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum."
     />
   );
 };
